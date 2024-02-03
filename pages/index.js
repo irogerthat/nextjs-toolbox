07e0dd0b5fef087d57f1ef5b3b0c80afddd0464b
Brian Keiseler <adbiks9@gmail.com>
2024-02-03 23:41:48 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Olivia Henderson</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="Olivia Henderson" />
         <hr />
-        <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
-        </p>
-        <FeedbackForm />
-        <JokeBlock />
       </main>
       <Footer />
     </div>
